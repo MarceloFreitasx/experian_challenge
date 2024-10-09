9c7be046c25a7206294143cc08b605868dc61473
Marcelo Freitas <marcelofreitasx@gmail.com>
2024-10-09 18:01:52 -0300

    test: fix character usecase test

--- test/unit/domain/usecases/characters_test.dart
+++ test/unit/domain/usecases/characters_test.dart
@@ -1,4 +1,5 @@
 import 'package:experian_challenge/domain/entities/entities.dart';
+import 'package:experian_challenge/domain/params/options.dart';
 import 'package:experian_challenge/domain/usecases/usecases.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -9,29 +10,30 @@ import '../../mocks.dart';
 void main() {
   group('GetCharactersListUseCase', () {
     late GetCharactersListUseCase sut;
+    final options = OptionsParams(limit: 2);
 
     setUp(() {
       sut = MockCharactersListUseCase();
     });
 
     test('should return a list of characters entity', () async {
-      when(() => sut.execute(limit: 2)).thenAnswer(
+      when(() => sut.execute(options)).thenAnswer(
         (_) async => [
           CharacterEntity(id: faker.randomGenerator.integer(1000000), name: faker.company.name()),
           CharacterEntity(id: faker.randomGenerator.integer(1000000), name: faker.company.name()),
         ],
       );
 
-      final entity = await sut.execute(limit: 2);
+      final entity = await sut.execute(options);
 
       expect(entity, isA<List<CharacterEntity>>());
       expect(entity.length, 2);
     });
 
     test('should throw an exception when usecase throws an exception', () async {
-      when(() => sut.execute(limit: 2)).thenThrow(Exception('Error fetching characters'));
+      when(() => sut.execute(options)).thenThrow(Exception('Error fetching characters'));
 
-      expect(() => sut.execute(limit: 2), throwsException);
+      expect(() => sut.execute(options), throwsException);
     });
   });
 }
